593ad0103c42c35342e6b73d3cbd06c7bf71933e
kirill221100 <kirulkacool@gmail.com>
2023-04-29 21:16:10 +0700

    fix

--- security/config.py
+++ security/config.py
@@ -7,5 +7,5 @@ load_dotenv('./.env')
 class Config:
     PSQL_URL = os.environ.get('PSQL_URL')
     ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES = 60
     SECRET_KEY = os.environ.get('SECRET_KEY')
